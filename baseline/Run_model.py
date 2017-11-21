5db467362711f7394f1b3629b6791f8af426132d
Ankita Mehta <ankitam1993@gmail.com>
2017-11-21 00:57:57 -0500

    features

--- baseline/Run_model.py
+++ baseline/Run_model.py
@@ -5,19 +5,33 @@ from SVM_RF import *
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_name', '-m',required = True,help="Pass the model name: rbf, SVM, nn")
 parser.add_argument('--arguments', '-a',help="Pass the arguments according to the model")
+parser.add_argument('--features', '-f',help="Pass the arguments y or n",required = True)
 
 args = parser.parse_args()
 
 model_name = args.model_name
 arguments = args.arguments
+bool = args.features
 
+print bool
 kargs = dict()
 
 if model_name == 'rbf':
     kargs['n_estimators'] = int(arguments)
 
+start = datetime.datetime.now()
 model = baseline_models()
 
-model.load_data()
-model.fit(model_name,kargs)
-model.predict()
+model.load_data(bool)
+
+if bool == 'y' or bool == 'Y':
+    print 'Extract HOG features'
+    model.make_features()
+
+model.fit(model_name,kargs,bool)
+model.predict(bool)
+
+end = datetime.datetime.now()
+print 'total time taken is : ', format(end-start)
+
+
--- baseline/SVM_RF.py
+++ baseline/SVM_RF.py
@@ -23,40 +23,48 @@ class baseline_models(object):
 
         self.model = None
 
-    def load_data(self):
+    def load_data(self,bool):
 
         X = []
         Y = []
+
+        print 'loading data'
 
         with open('../data/fer2013.csv', 'r') as f:
             data = csv.reader(f)
 
             for row in data:
-                Y.append(row[0])
-                z = list(row[1:-1][0].split())
-                X.append(z)
 
-        X = [a for a in X[1:]]
-        Y = [a for a in Y[1:]]
+                if row[0] != 'emotion':
+                    Y.append(int(row[0]))
+                    z = list(row[1:-1][0].split())
+                    z = [int(a) for a in z]
+                    X.append(z)
+                else:
+                    print row
 
         total_l = len(X)
 
-        self.X_train = X[0:80*total_l/100]
-        self.Y_train = Y[0:80*total_l/100]
+        self.X_train = np.asarray(X[0:80 * total_l / 100], dtype=np.float32)
+        self.Y_train = np.asarray(Y[0:80 * total_l / 100], dtype=np.int)
 
-        self.X_val = X[80*total_l/100:90*total_l/100]
-        self.Y_val = Y[80*total_l/100:90*total_l/100]
+        self.X_val = np.asarray(X[80 * total_l / 100:90 * total_l / 100], dtype=np.float32)
+        self.Y_val = np.asarray(Y[80 * total_l / 100:90 * total_l / 100], dtype=np.int)
 
-        self.X_test = X[90*total_l/100:total_l]
-        self.Y_test = Y[90*total_l/100:total_l]
+        self.X_test = np.asarray(X[90 * total_l / 100:total_l], dtype=np.float32)
+        self.Y_test = np.asarray(Y[90 * total_l / 100:total_l], dtype=np.int)
 
-        print 'length of x train and y train:' , len(self.X_train) , len(self.Y_train)
+        if bool == 'y' or bool == 'Y':
+            self.X_train = self.X_train.reshape(self.X_train.shape[0], 48, 48)
+            self.X_val = self.X_val.reshape(self.X_val.shape[0], 48, 48)
+            self.X_test = self.X_test.reshape(self.X_test.shape[0], 48, 48)
+
+        print 'length of x train and y train:', self.X_train.shape, self.Y_train.shape
         print 'data loaded'
 
-    def features(self):
+    def make_features(self):
 
-        num_color_bins = 10  # Number of bins in the color histogram
-        feature_fns = [hog_feature, lambda img: color_histogram_hsv(img, nbin=num_color_bins)]
+        feature_fns = [hog_feature]
         X_train_feats = extract_features(self.X_train, feature_fns, verbose=True)
         X_val_feats = extract_features(self.X_val, feature_fns)
         X_test_feats = extract_features(self.X_test, feature_fns)
@@ -79,24 +87,54 @@ class baseline_models(object):
         self.X_val_feats = np.hstack([X_val_feats, np.ones((X_val_feats.shape[0], 1))])
         self.X_test_feats = np.hstack([X_test_feats, np.ones((X_test_feats.shape[0], 1))])
 
-    def fit(self,model_name,kwargs):
+        print self.X_train_feats.shape
+
+        print 'feature extraction done'
+        # print X_train_feats[0]
+
+        # print X_val_feats[0]
+
+        # print X_test_feats[0]
+
+    def fit(self,model_name,kwargs,bool):
         if model_name == 'svm':
+            print 'SVM MODEL'
             self.model = svm.SVC()
 
         elif model_name == 'rbf':
+            print 'RBF Model'
             self.model = RandomForestClassifier(**kwargs)
 
         print 'fitting the model...'
-        self.model.fit(self.X_train, self.Y_train)
+
+        if bool == 'y' or bool == 'Y':
+            print 'with HOG features'
+            self.model.fit(self.X_train_feats, self.Y_train)
+
+        else:
+
+            self.model.fit(self.X_train, self.Y_train)
 
         print 'model fitting done...'
 
-    def predict(self):
+    def predict(self,bool):
 
         print 'predicting the model'
-        y_pred = self.model.predict(self.X_test)
+
+        if bool == 'y' or bool == 'Y':
+            print 'with HOG features'
+            y_pred = self.model.predict(self.X_test_feats)
+
+            y_val_pred = self.model.predict(self.X_val_feats)
+
+        else:
+
+            y_pred = self.model.predict(self.X_test)
+            y_val_pred = self.model.predict(self.X_val)
 
         print 'Test set accuracy: ', (self.Y_test == y_pred).mean()
+
+        print 'Validation set accuracy: ', (self.Y_val == y_val_pred).mean()
 
 
 
